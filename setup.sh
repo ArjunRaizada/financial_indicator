a819b91849d8bcd253f8b405a46c0c4dda9912aa
ArjunRaizada <103138549+ArjunRaizada@users.noreply.github.com>
2024-11-15 23:03:04 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 
-# Download and extract TA-Lib
-echo "Downloading TA-Lib source..."
-wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
-tar -xzvf ta-lib-0.4.0-src.tar.gz
+# Upgrade pip to the latest version
+echo "Upgrading pip..."
+pip install --upgrade pip
 
-# Navigate to TA-Lib directory, configure, make, and install
-cd ta-lib
-echo "Configuring and installing TA-Lib..."
-./configure --prefix=/usr
-make
-sudo make install
-cd ..
-
-# Install TA-Lib Python package
-echo "Installing TA-Lib Python package..."
-pip install TA-Lib
-
-# Install other Python packages
+# Install required Python packages from requirements.txt
 echo "Installing required Python packages..."
-pip install streamlit pandas numpy joblib yfinance scikit-learn gdown ta-lib
+pip install -r requirements.txt
 
 echo "Setup completed successfully!"
