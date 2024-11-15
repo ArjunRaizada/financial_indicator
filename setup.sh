defc5b8f4536c4b4a8b60bbc1b484ae8dc4b5718
ArjunRaizada <103138549+ArjunRaizada@users.noreply.github.com>
2024-11-15 21:26:18 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
-
-# Update package list and install build dependencies
 sudo apt-get update
-sudo apt-get install -y build-essential wget
-
-# Download and install TA-Lib
+sudo apt-get install -y build-essential libxml2-dev libxslt1-dev
 wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
 tar -xzvf ta-lib-0.4.0-src.tar.gz
 cd ta-lib
@@ -12,7 +7,4 @@ cd ta-lib
 make
 sudo make install
 cd ..
-rm -rf ta-lib ta-lib-0.4.0-src.tar.gz
-
-# Now install Python dependencies
-pip install -r requirements.txt
+pip install TA-Lib
