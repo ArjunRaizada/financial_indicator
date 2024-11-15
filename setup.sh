fdc297002b7a7fa41cd705d479808290a18aeeaa
ArjunRaizada <103138549+ArjunRaizada@users.noreply.github.com>
2024-11-15 21:46:39 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,24 @@
-!pip install streamlit
-!pip install yfinance
-!pip install joblib
-!pip install TA-Lib
-!pip install scikit-learn
+#!/bin/bash
 
-# Install ta-lib if it's not already installed
-!wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
-!tar -xzvf ta-lib-0.4.0-src.tar.gz
-import os
-os.chdir('ta-lib')
-!./configure --prefix=/usr
-!make
-!make install
-os.chdir('../')
-!pip install TA-Lib
+# Download and extract TA-Lib
+echo "Downloading TA-Lib source..."
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xzvf ta-lib-0.4.0-src.tar.gz
+
+# Navigate to TA-Lib directory, configure, make, and install
+cd ta-lib
+echo "Configuring and installing TA-Lib..."
+./configure --prefix=/usr
+make
+sudo make install
+cd ..
+
+# Install TA-Lib Python package
+echo "Installing TA-Lib Python package..."
+pip install TA-Lib
+
+# Install other Python packages
+echo "Installing required Python packages..."
+pip install streamlit pandas numpy joblib yfinance scikit-learn
+
+echo "Setup completed successfully!"
